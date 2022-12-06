8d2db08198eb5c42f378f6bdfa270d52f792b2f0
imaspacecat <teamrobotics8872@gmail.com>
2022-12-06 15:27:01 -0800

    added HardCodedAutonomous.java
    -Kosei

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/HardCodedAutonomous.java
@@ -0,0 +1,31 @@
+/*
+thx 8872 for code
+https://github.com/8872/8872PP/blob/finalWithAutonomous/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/drive/brinopmodes/LeftAuto.java
+ */
+
+
+package org.firstinspires.ftc.teamcode.autonomous;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.arcrobotics.ftclib.hardware.SimpleServo;
+import com.arcrobotics.ftclib.hardware.motors.MotorEx;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.TouchSensor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import org.firstinspires.ftc.teamcode.roadrunner.drive.DriveConstants;
+import org.firstinspires.ftc.teamcode.roadrunner.drive.SampleMecanumDrive;
+import org.firstinspires.ftc.teamcode.roadrunner.trajectorysequence.TrajectorySequence;
+import org.firstinspires.ftc.teamcode.roadrunner.trajectorysequence.TrajectorySequenceBuilder;
+import org.firstinspires.ftc.teamcode.subsystem.ArmSubsystem;
+
+@Config
+@Autonomous
+public class HardCodedAutonomous extends LinearOpMode {
+
+    public void runOpMode() throws InterruptedException {
+
+    }
+}
