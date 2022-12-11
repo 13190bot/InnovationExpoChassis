0fd4419b19909329fd4259593695f4a079b68478
suri312006 <suri312006@gmail.com>
2022-12-10 22:30:09 -0800

    HOLY THIS IS FUCKIN WORKING, enum always left though

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/vision/VisionTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/vision/VisionTest.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.autonomous.roadrunner.vision;
+package org.firstinspires.ftc.teamcode.autonomous.vision;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
@@ -8,10 +8,11 @@ import org.openftc.easyopencv.OpenCvCamera;
 import org.openftc.easyopencv.OpenCvCameraFactory;
 import org.openftc.easyopencv.OpenCvCameraRotation;
 
-@Autonomous(name = "Signal Sleeve Test")
+
+@Autonomous(name = "Vision Test")
 public class VisionTest extends LinearOpMode {
 
-    SleeveDetection sleeveDetection;
+    org.firstinspires.ftc.teamcode.autonomous.roadrunner.vision.SleeveDetection sleeveDetection;
     OpenCvCamera camera;
 
     // Name of the Webcam to be set in the config
@@ -21,7 +22,7 @@ public class VisionTest extends LinearOpMode {
     public void runOpMode() throws InterruptedException {
         int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
         camera = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, webcamName), cameraMonitorViewId);
-        sleeveDetection = new SleeveDetection();
+        sleeveDetection = new org.firstinspires.ftc.teamcode.autonomous.roadrunner.vision.SleeveDetection();
         camera.setPipeline(sleeveDetection);
 
         camera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener()
