0092ac479ffe8bc36443546a0fe0e56ed599d588
suri312006 <suri312006@gmail.com>
2022-12-11 11:26:34 -0800

    finished test manual auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/manualAuto/ManualOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/manualAuto/ManualOpMode.java
@@ -3,6 +3,10 @@ package org.firstinspires.ftc.teamcode.autonomous.manualAuto;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.teamcode.autonomous.vision.SleeveDetection;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
 
 import static android.os.SystemClock.sleep;
 
@@ -12,8 +16,10 @@ import static android.os.SystemClock.sleep;
 public class ManualOpMode extends OpMode {
 
     DcMotor lf, lb, rf, rb;
+    SleeveDetection sleeveDetection;
+    OpenCvCamera camera;
 
-    Camera camera;
+    String webcamName = "Webcam 1";
 
     @Override
     public void init() {
@@ -33,23 +39,43 @@ public class ManualOpMode extends OpMode {
 
         telemetry.addData("init", "done");
 
+
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        camera = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, webcamName), cameraMonitorViewId);
+        sleeveDetection = new SleeveDetection();
+        camera.setPipeline(sleeveDetection);
+        camera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener()
+        {
+            @Override
+            public void onOpened()
+            {
+                camera.startStreaming(1280,720);
+            }
+
+            @Override
+            public void onError(int errorCode) {}
+        });
+
+        telemetry.addData("Position: ", sleeveDetection.getPosition());
+        telemetry.update();
+
     }
 
     @Override
     public void start () {
 
         //strafe to face park pos
-        switch ( camera::getParkPos ) {
-            case 1: //left
+        switch ( sleeveDetection.getPosition()) {
+            case LEFT: //left
                 rf.setPower(.5);
                 rb.setPower(-.5);
                 lf.setPower(-.5);
                 lb.setPower(.5);
                 sleep(3000);
                 break;
-            case 2: //if the middle parkpos
+            case CENTER: //if the middle parkpos
                 break;
-            case 3: //right
+            case RIGHT: //right
                 rf.setPower(-.5);
                 rb.setPower(.5);
                 lf.setPower(.5);
