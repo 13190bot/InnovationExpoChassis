ffb1f9cb36dabf6fea7f3f15b4164867bca8d663
suri312006 <suri312006@gmail.com>
2022-12-12 22:45:49 -0800

    finished normal teleop, with drive only and arm only versions.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/normal/NormalDriveTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/normal/NormalDriveTeleOp.java
@@ -4,23 +4,17 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.arcrobotics.ftclib.hardware.ServoEx;
-import com.arcrobotics.ftclib.hardware.SimpleServo;
 
-@TeleOp (name = "Normie TeleOp")
-public class NormalTeleOp extends OpMode {
+@TeleOp (name = "Normie Drive TeleOp")
+public class NormalDriveTeleOp extends OpMode {
 
-    DcMotor motorFrontLeft;
-    DcMotor motorFrontRight;
-    DcMotor motorBackLeft;
-    DcMotor motorBackRight;
-    static DcMotor slideL;
-    static DcMotor slideR;
-    ServoEx claw;
+    DcMotor motorFrontLeft, motorFrontRight, motorBackLeft, motorBackRight;
+    boolean slowTime = gamepad1.left_bumper;
     double mul = 1;
+
     double turnSpeed = 0.5;
-    double slideSpeed = 0.7;
     public void init () {
+
         motorFrontLeft = hardwareMap.dcMotor.get("leftFront");
         motorBackLeft = hardwareMap.dcMotor.get("leftBack");
         motorFrontRight = hardwareMap.dcMotor.get("rightFront");
@@ -38,37 +32,10 @@ public class NormalTeleOp extends OpMode {
         motorBackLeft.setDirection(DcMotorSimple.Direction.REVERSE);
         motorBackRight.setDirection((DcMotorSimple.Direction.REVERSE));
 
-
-
-        slideL = hardwareMap.dcMotor.get("slideL");
-        slideR = hardwareMap.dcMotor.get("slideR");
-
-        DcMotor[] slides = {slideL, slideR};
-        for(DcMotor slide : slides) {
-            slide.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-            slide.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-            slide.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        }
-
-
         telemetry.addData("init", "done");
 
     }
     public void loop () {
-
-        //claw manip
-        if(gamepad2.right_bumper){claw.setPosition(1);}
-        if(gamepad2.left_bumper){claw.setPosition(0);}
-
-        //lift gonna be manual (if parteek want set junctions, he can write them himself)
-        if(gamepad2.dpad_up){
-            slideR.setPower(slideSpeed);
-            slideL.setPower(slideSpeed);
-        }
-        if(gamepad2.dpad_down){
-            slideR.setPower(-slideSpeed);
-            slideL.setPower(-slideSpeed);
-        }
 
         //run drive
         double Y = -gamepad1.left_stick_y; // Remember, this is reversed!
@@ -76,7 +43,11 @@ public class NormalTeleOp extends OpMode {
         double rx = gamepad1.right_trigger;
         double lx = gamepad1.left_trigger;
 
-        boolean slowTime = gamepad1.left_bumper;
+        /*
+        a -> slowmode implemented with just multiplying power
+        b -> slowmode implemented with clipping joystick output
+         */
+
 
         if (slowTime) {
             mul = 0.5;
@@ -98,6 +69,8 @@ public class NormalTeleOp extends OpMode {
             frontLeftPower = backLeftPower = -turnSpeed;
             frontRightPower = backRightPower = turnSpeed;
         }
+
+
 
         frontLeftPower *= mul;
         backLeftPower *= mul;
