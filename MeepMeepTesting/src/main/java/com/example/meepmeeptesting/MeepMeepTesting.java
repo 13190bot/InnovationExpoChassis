a87579c0380a9a9ddf3bff33fbcc3693e52440a2
suri312006@gmail.com <suri312006@gmail.com>
2023-01-03 06:23:39 -0800

    working on hihg junction + park auto (in progress)

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -1,8 +1,10 @@
 package com.example.meepmeeptesting;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
+import com.noahbres.meepmeep.roadrunner.SampleMecanumDrive;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
 public class MeepMeepTesting {
@@ -13,10 +15,18 @@ public class MeepMeepTesting {
                 // TODO Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width that match our actual bot
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
                 .followTrajectorySequence(drive ->
-                        drive.trajectorySequenceBuilder(new Pose2d(35, -58.333333, Math.toRadians(90)))
-                                .forward(23.333333)
-                                .turn(Math.toRadians(90))
-                                .forward(23.333333)
+                        drive.trajectorySequenceBuilder(new Pose2d(0, -58.333333, Math.toRadians(90)))
+                                //grab cone
+                                //.forward(23.333333*2)
+                                //.splineTo(new Vector2d(34.5,-11.6), Math.toRadians(135))
+                                .lineToConstantHeading(new Vector2d(34.5,-11.6))
+                                //slide up to high junction
+                                .turn(Math.toRadians(45))
+                                .forward(3)
+                                //drop cone
+                                .forward(-3)
+                                .turn(Math.toRadians(-45))
+                                //slides down
                                 .build()
                 );
 
