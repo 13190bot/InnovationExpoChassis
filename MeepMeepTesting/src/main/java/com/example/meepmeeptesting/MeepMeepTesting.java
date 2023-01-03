f31a7553f92134d70de957835f4d4b6c60952d09
suri312006@gmail.com <suri312006@gmail.com>
2023-01-02 18:16:55 -0800

    finished roadrunner park auto, changed motor names in SampleMecanumDrive

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -10,18 +10,13 @@ public class MeepMeepTesting {
         MeepMeep meepMeep = new MeepMeep(800);
 
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
-                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                // TODO Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width that match our actual bot
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
                 .followTrajectorySequence(drive ->
-                        drive.trajectorySequenceBuilder(new Pose2d(0, 0, 0))
-                                .forward(30)
+                        drive.trajectorySequenceBuilder(new Pose2d(35, -58.333333, Math.toRadians(90)))
+                                .forward(23.333333)
                                 .turn(Math.toRadians(90))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
+                                .forward(23.333333)
                                 .build()
                 );
 
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/roadrunner/drive/opmode/Surendra/DriveLeft.java
@@ -0,0 +1,4 @@
+package org.firstinspires.ftc.teamcode.autonomous.roadrunner.drive.opmode.Surendra;
+
+public class DriveLeft {
+}
