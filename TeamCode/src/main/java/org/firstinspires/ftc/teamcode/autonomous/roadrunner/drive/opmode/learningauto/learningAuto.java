f5c39909473c2300c962b3f8585c3ab94694f1e9
RichardkimAXel <taeungart@gmail.com>
2023-01-24 15:55:40 -0800

    added meepmeep test

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/roadrunner/drive/opmode/learningauto/learningAuto.java
@@ -0,0 +1,29 @@
+package org.firstinspires.ftc.teamcode.autonomous.roadrunner.drive.opmode.learningauto;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import org.firstinspires.ftc.teamcode.autonomous.roadrunner.drive.SampleMecanumDrive;
+
+@Autonomous(name = "Test")
+public class learningAuto extends LinearOpMode {
+
+    Pose2d startingPos = new Pose2d(0,0,Math.toRadians(180)); //figure out correct to rad.
+
+    private SampleMecanumDrive drive;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        //drive setup
+        drive = new SampleMecanumDrive(hardwareMap);
+
+
+        waitForStart();
+
+        drive.followTrajectorySequenceAsync(drive.trajectorySequenceBuilder(startingPos)
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+                .build()
+        );
+    }
+
+}
