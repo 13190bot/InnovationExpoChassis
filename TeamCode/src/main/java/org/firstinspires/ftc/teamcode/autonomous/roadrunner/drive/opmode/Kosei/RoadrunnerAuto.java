4b807eb02d142e1f841a897ee16b4b839a73630d
imaspacecat <teamrobotics8872@gmail.com>
2023-01-24 17:22:25 -0800

    auto5 -kosei

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/roadrunner/drive/opmode/Kosei/RoadrunnerAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/roadrunner/drive/opmode/Kosei/RoadrunnerAuto.java
@@ -25,9 +25,74 @@ public class RoadrunnerAuto extends LinearOpMode {
                 /*
                 1 tile is 18 inches
                  */
+
+
+                // INIT
+
+
+
+                // go to cone stack
+                /*
                 .forward(46.5)
                 .turn(Math.toRadians(-90))
                 .forward(27)
+                 */
+
+                .lineToLinearHeading(new Pose2d(35, -58.333333 + 46.5, Math.toRadians(0)))
+                .forward(27)
+
+                //.splineToLinearHeading(new Pose2d(35 + 27, -58.333333 + 46.5, Math.toRadians(0)), Math.toRadians(-180))
+
+
+
+
+
+
+
+
+
+
+
+                // LOOP
+
+
+
+                // grab cone
+
+
+
+                // go to high junction
+                /*
+                .back(27 + 11.5)
+                .turn(Math.toRadians(90))
+                .forward(5)
+                 */
+                .lineToLinearHeading(new Pose2d(35 + 27 - (27 + 11.5), -58.333333 + 46.5, Math.toRadians(90)))
+                .forward(5)
+
+                // set lift height to high
+
+
+
+                // drop cone
+
+
+
+                // go back a bit so we don't put claw on junction
+                /*
+                .back(5)
+                */
+                .lineToLinearHeading(new Pose2d(35 + 27 - (27 + 11.5), -58.333333 + 46.5, Math.toRadians(0)))
+
+                // set lift height to ground
+
+
+
+                // go back to cone stack
+                //.turn(Math.toRadians(-90))
+                .forward(27 + 11.5)
+
+
                 .build()
         );
     }
