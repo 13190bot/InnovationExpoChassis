1b6e56802ff31ddef718f3e4765aba3db7496896
cryptopig <71409503+cryptopig@users.noreply.github.com>
2023-02-09 22:42:41 -0800

    created 1+0

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/manualAuto/oneZeroAuto.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode.autonomous.manualAuto;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@Autonomous(name = "Preload_Auto")
+public class oneZeroAuto extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+    }
+}
