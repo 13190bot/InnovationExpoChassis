ec3d28a348e5a5b00f1c0e658c8fc44d0bb4856a
devmello <kol.kid.py@gmail.com>
2023-03-30 21:36:30 -0700

    SPED THE BOT THE HELL UP. 1000 HZ TO 5000HZ REFRESH LATE

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/logger/Logger.java
+++
@@ -1,9 +0,0 @@
-package org.firstinspires.ftc.teamcode.logger;
-
-import com.qualcomm.robotcore.hardware.HardwareMap;
-
-public class Logger {
-    Logger(HardwareMap hardwareMap, String[] deviceNames){
-
-    }
-}
